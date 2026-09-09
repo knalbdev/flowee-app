43859dc6693808ab564fe9721b8a65f646b3960a
Setianing Budi <setianinghem2>
2026-09-09 10:44:16 +0700

    adding detail header widget

--- lib/widgets/circle_icon_button.dart
+++ lib/widgets/circle_icon_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // tombol bulat, dengan ikon di tengahnya, yang muncul di beberapa screen
 class CircleIconButton extends StatelessWidget {
-  const new({super.key, required this.icon, required this.onTap, required this.iconColor});
+  const new({super.key, required this.icon, required this.onTap, this.iconColor = Colors.black87});
 
   final IconData icon;
   final VoidCallback onTap;
