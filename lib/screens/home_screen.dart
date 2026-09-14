f6536e18eb4c43bc2480072cac57990785e56557
Setianing Budi <setianinghem2>
2026-09-14 08:40:49 +0700

    finishing home screen UI Definition

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flowee_app/data/dummy_data.dart';
 import 'package:flowee_app/models/flower.dart';
 import 'package:flowee_app/screens/detail_screen.dart';
-import 'package:flowee_app/widgets/home_header.dart';
+import 'package:flowee_app/widgets/flower_card.dart';
+import 'package:flowee_app/widgets/home_content_header.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -43,10 +44,37 @@ class _HomeScreenState extends State<HomeScreen> {
       child: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-            child: HomeHeader(
-              _selectedCategory: 
-            ),
-          )
+            child: HomeContentHeader(
+              selectedCategory: _selectedCategory,
+              categories: _categories,
+              onQueryChanged: (value) => setState(() => _query = value),
+              onCategorySelected: (value) => setState(() => _selectedCategory = value),
+            )
+          ),
+          if (flowers.isEmpty)
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(child: Text('Bunga Tidak Ditemukan')),
+            )
+          else
+            SliverPadding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 100),
+              sliver: SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  childAspectRatio: 0.68
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => FlowerCard(
+                    flower: flowers[index],
+                    onTap: () => _openDetail(flowers[index]),
+                  ),
+                  childCount: flowers.length
+                ),
+              ),
+            )
         ],
       ),
     );
